2675cb47d28dbc54abfa17cf6703576e0f30afa3
CookieMC337 <51511368+CookieMC337@users.noreply.github.com>
2024-03-27 17:36:12 +0100

    Update RelationFinder.php

--- src/RelationFinder.php
+++ src/RelationFinder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BeyondCode\ErdGenerator;
+namespace CookieMC337\ErdGenerator;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
